4ac8342e50c0c587157ea736167b28361a0586b0
Tommy Evans-Barton <tommy1631@gmail.com>
2026-09-08 17:13:07 -0700

    - Creation of precipitation mapping json - Addition of action and yard prediction tracking in State/drive simulation

--- src/State.py
+++ src/State.py
@@ -5,5 +5,8 @@ class State:
         self.yardline_100 = yardline_100
         self.covars = covars
 
+        self.action_taken = None
+        self.yards_predicted = None
+
     def __str__(self):
-        return 'Down: {d}, Yard to Go: {y}, Yardline: {l}'.format(d = self.down, y = self.ydstogo, l = self.yardline_100)
+        return 'Down: {d}, Yard to Go: {y}, Yardline: {l} \nAction Predicted: {a}, Yards Predicted: {yp}'.format(d = self.down, y = self.ydstogo, l = self.yardline_100, a = self.action_taken, yp = self.yards_predicted)
